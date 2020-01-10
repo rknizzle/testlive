ca8a91d48b7432e9b411b14a70f85a882ccac380
rknizzle <ryan@autotiv.com>
2020-01-09 22:50:26 -0500

    A basic datastore wrapper that all datastore methods will "inherit" from

---
+++ datastore/datastore.go
@@ -0,0 +1,14 @@
+// Package datastore is a data store abstraction in which all data store methods will be a subpackage of
+
+package datastore
+
+import (
+	"github.com/rknizzle/testlive/job"
+)
+
+type Datastore interface {
+	Create(*job.Job) *job.Job
+	Update(string, *job.Job) (*job.Job, error)
+	Get(string) (*job.Job, error)
+	GetAll() []*job.Job
+}
